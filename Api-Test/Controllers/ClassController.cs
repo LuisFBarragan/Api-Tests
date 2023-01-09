78efc03d165d6b1e67104bf8836866d9d4e05cdd
luisf <luisf@DESKTOP-JJ6THBO>
2023-01-09 03:44:37 -0600

    Agregar archivos de proyecto.

---
+++ Api-Test/Controllers/ClassController.cs
@@ -0,0 +1,22 @@
+﻿using DA;
+using DA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api_Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClassController
+    {
+        [HttpGet("GetVista")]
+
+        public async Task<string> GetVista()
+        {
+            TestProviders vista = new TestProviders();
+            IEnumerable<vAdditionalContactInfo> resultado = await vista.LeerVista();
+            return JsonConvert.SerializeObject(resultado);
+        }
+    }
+}
+
---
+++ DA/TestProviders.cs
@@ -0,0 +1,29 @@
+﻿using DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA
+{
+    public class TestProviders
+    {
+        public async Task<IEnumerable<vAdditionalContactInfo>> LeerVista()
+        {
+            try
+            {
+                using (Entities context = new Entities())
+                {
+
+                    return await context.vAdditionalContactInfo.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
